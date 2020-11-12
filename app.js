55893cc7f4b48267141e083b75bb990884a524dd
AleksandrS <ASagalajev92@gmail.com>
2020-11-12 15:13:30 +0200

    hash password by bcrypt package

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -50,14 +51,21 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
     const userEmail = req.body.userEmail;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
+    //-----    Search in DB by entered email   -----//
     User.findOne({ email: userEmail }, function(err, foundUser) {
         if (!err) {
-            if (foundUser.password === password) {
-                res.render("secrets", {
-                    userName: foundUser.name
+            if (foundUser) {
+                //-----    Hash entered text and compare with saved hash in DB   -----//
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets", {
+                            userName: foundUser.name
+                        });
+                    }
                 });
+
             }
         } else {
             res.render("home");
@@ -71,28 +79,31 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        name: req.body.userName,
-        email: req.body.userEmail,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+        const newUser = new User({
+            name: req.body.userName,
+            email: req.body.userEmail,
+            password: hash
+        });
+
+        newUser.save(function(err) {
+            if (!err) {
+                res.render("secrets", {
+                    userName: newUser.name
+                });
+            } else {
+                console.log(err);
+            }
+        });
     });
 
-    newUser.save(function(err) {
-        if (!err) {
-            res.render("secrets", {
-                userName: newUser.name
-            });
-        } else {
-            console.log(err);
-        }
-    });
 });
 
 app.get("/logout", function(req, res) {
     res.render("home");
 });
-
-
 
 
 
